f21608c570b68898c64fb86288fa8735d6bc64f2
Benoit Demonsant & Karate Kid <pair+keops6fr.sbsample@devbootcamp.com>
2015-06-15 16:50:27 -0700

    first commit

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,51 @@
 var newTodoList = function() {
-	// ???
+	this.tasks = [];
+};
+
+newTodoList.prototype.add = function(item){
+  this.tasks.push(new Task(item, (this.tasks.length +1)))
+};
+
+newTodoList.prototype.list = function(){
+  for (i=0; i < this.tasks.length; i++){
+    console.log(this.tasks[i]);
+  };
+};
+
+var Task = function (description, id) {
+  this.id = id
+  this.description = description;
+  this.completed = false;
 };
 
 
+Task.prototype.complete = function () {
+  this.completed = true;
+};
 
+Task.prototype.remove = function () {
+  this.delete
+  console.log("Removed");
+};
 // Driver code
 
 
-var todoList = newTodoList();
+var groceryList = new newTodoList();
+groceryList.add('apple');
+groceryList.add('banana');
+groceryList.add('orange');
+groceryList.list();
+
+var breadTask = groceryList.tasks[0];
+
+console.log(breadTask.id); //-> 1 (some unique numerical ID)
+console.log(breadTask.description); //-> 'bread'
+console.log(breadTask.completed); //-> false
+breadTask.complete();
+console.log(breadTask.completed);
+
+groceryList.list();
+
+breadTask.remove();
+
+
